b9f257a85b0ab2e1e1270a33ebba9d13459e5f1a
Jorge Islas <jorgealberto.pineda78@gmail.com>
2026-08-09 16:52:58 -0600

    Agregando practica 1 y proyecto hola

---
+++ Practica1/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static int suma(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(
+        g[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        // Demostración de la función suma
+        int a = 3;
+        int b = 4;
+        System.out.println("suma(" + a + ", " + b + ") = " + suma(a, b));
+    }
+}
---
+++ hola/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+    public static int suma(int a, int b) {
+        return a + b;
+    }
+}
